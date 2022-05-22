c8259366c1af41a9416866f2ef2a6327eb704e99
HarshitaKachhadiya <84308506+HarshitaKachhadiya@users.noreply.github.com>
2022-05-22 16:27:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+import Carousel from './components/Carousel';
+import CreamSection from './components/CreamSection';
+import Header from './components/Header';
+import Section from './components/Section';
+import SmallSection from './components/SmallSection';
+import { Data } from './components/Data';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <SmallSection />
+      <CreamSection />
+      <Carousel slides={Data}/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
